97a9289353354182aad36dbf1979b019bb0cc9ab
Agboola Vincent <agboolavin65@gmail.com>
2024-05-28 23:10:05 +0100

    Added navbar component to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import NavBar from "./NavBar";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      Home page
+      <NavBar />
     </main>
   );
 }
