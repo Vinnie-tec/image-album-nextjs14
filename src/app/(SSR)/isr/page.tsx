b3dfba7a534a1e7aeddc27807fe0faf0c54b5d3e
Agboola Vincent <agboolavin65@gmail.com>
2024-05-29 10:37:17 +0100

    Added ISR page

---
+++ src/app/(SSR)/isr/page.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { UnsplashImage } from "@/models/unsplash-image";
+import Image from "next/image";
+import Link from "next/link";
+import { Alert } from "@/components/bootstrap";
+
+export const metadata = {
+  title: "Incremental Static Regeneration Fetching - Image Album",
+};
+
+export const revalidate = 0;
+
+const ISR = async () => {
+  const response = await fetch(
+    "https://api.unsplash.com/photos/random?client_id=" +
+      process.env.UNSPLASH_ACCESS_KEY,
+    {
+      // cache: "no-cache"/"no-store" OR
+      // next: { revalidate: 0 }
+    }
+  );
+  const image: UnsplashImage = await response.json();
+
+  const width = Math.min(500, image.width);
+  const height = (width / image.width) * image.height;
+
+  return (
+    <div className="d-flex flex-column align-items-center">
+      <Alert>
+        This page uses <strong>incremental static regeneration</strong>. A new
+        image is fetched every 15 seconds (after refreshing the page) and then
+        served from the cache for that duration.
+      </Alert>
+      
+      <Image
+        src={image.urls.raw}
+        width={width}
+        height={height}
+        alt={image.description}
+        className="rounded shadow mw-100 h-100"
+      />
+      by{" "}
+      <Link href={"/users/" + image.user.username}>{image.user.username}</Link>
+    </div>
+  );
+};
+
+export default ISR;
