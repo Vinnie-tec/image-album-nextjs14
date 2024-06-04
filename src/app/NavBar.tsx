bf36d293df73c84b623e0dfcda5cd8503bbe7937
Agboola Vincent <agboolavin65@gmail.com>
2024-06-04 07:03:34 +0100

    Added dropdown to nav bar

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,27 +1,54 @@
 "use client";
 
 import Link from "next/link";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { usePathname } from "next/navigation";
 
 export default function NavBar() {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    return (
-        <Navbar bg="primary" variant="dark" sticky="top" expand="sm" collapseOnSelect>
-            <Container>
-                <Navbar.Brand as={Link} href="/">
-                    Vinnie Image Gallery
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="main-navbar" />
-                <Navbar.Collapse id="main-navbar">
-                    <Nav>
-                        <Nav.Link as={Link} href="/static" active={pathname === "/static"}>Static</Nav.Link>
-                        <Nav.Link as={Link} href="/dynamic" active={pathname === "/dynamic"}>Dynamic</Nav.Link>
-                        <Nav.Link as={Link} href="/isr" active={pathname === "/isr"}>ISR</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar
+      bg="primary"
+      variant="dark"
+      sticky="top"
+      expand="sm"
+      collapseOnSelect
+    >
+      <Container>
+        <Navbar.Brand as={Link} href="/">
+          Vinnie Image Gallery
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-navbar" />
+        <Navbar.Collapse id="main-navbar">
+          <Nav>
+            <Nav.Link as={Link} href="/static" active={pathname === "/static"}>
+              Static
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              href="/dynamic"
+              active={pathname === "/dynamic"}
+            >
+              Dynamic
+            </Nav.Link>
+            <Nav.Link as={Link} href="/isr" active={pathname === "/isr"}>
+              ISR
+            </Nav.Link>
+            <NavDropdown title="Topics" id="topics-dropdown">
+              <NavDropdown.Item as={Link} href="/topics/health">
+                Health
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} href="/topics/fitness">
+                Fitness
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} href="/topics/coding">
+                Coding
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
